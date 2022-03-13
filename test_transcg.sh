aa24ad2c08ebc60717a996d70d414c265bdf6353
ZerenYu <yuzeren@umich.edu>
2022-03-13 18:51:25 -0400

    init

---
+++ test_transcg.sh
@@ -0,0 +1,6 @@
+python clearpose/TransCG/test.py --cfg /home/yuzeren/benchmarks/ClearPose/clearpose/TransCG/configs/default.yaml --test standard_test.csv
+python clearpose/TransCG/test.py --cfg /home/yuzeren/benchmarks/ClearPose/clearpose/TransCG/configs/default.yaml --test occlusion_test.csv
+python clearpose/TransCG/test.py --cfg /home/yuzeren/benchmarks/ClearPose/clearpose/TransCG/configs/default.yaml --test wou_test.csv
+python clearpose/TransCG/test.py --cfg /home/yuzeren/benchmarks/ClearPose/clearpose/TransCG/configs/default.yaml --test color_test.csv
+python clearpose/TransCG/test.py --cfg /home/yuzeren/benchmarks/ClearPose/clearpose/TransCG/configs/default.yaml --test covered_test.csv
+python clearpose/TransCG/test.py --cfg /home/yuzeren/benchmarks/ClearPose/clearpose/TransCG/configs/default.yaml --test non-planner_test.csv
